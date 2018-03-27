a35731d4de929c106508b4e58ac913fae1b7217f
jwiley84 <jess.m.wailes@gmail.com>
2018-03-27 12:03:07 -0700

    inital commit of Classic Arcade Files

---
+++ apps/scoreKeeper/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect, HttpResponse
+import json
+
+# Create your views here.
+def index(request):
+    print "FFS, WORK DAMN YOU"
+    return render(request, 'scoreKeeper/index.html')
+
+def receiver(request):
+    # print "I saw this"
+    # data.request.get_json()
+    # results = ""
+
+    # for item in data:
+    #     results += str(item['make']) + '\n'
+    # print results
+    print "this button works"
+    return render(request, 'scoreKeeper/breakoutGame.html')
